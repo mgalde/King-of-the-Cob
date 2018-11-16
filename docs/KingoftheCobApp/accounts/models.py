eb640f1bab836a2aa365f5d101426dd5b6f30c8f
Michael <30883926+mgalde@users.noreply.github.com>
2018-11-15 18:31:32 -0600

    model views

--- docs/KingoftheCobApp/accounts/models.py
+++ docs/KingoftheCobApp/accounts/models.py
@@ -4,3 +4,19 @@ from datetime import date
 from django.utils.html import escape, mark_safe
 
 # Create your models here.
+
+class Task(models.Model):
+    """Model representing a Event or Task."""
+    name = models.CharField(max_length=200, help_text='Enter a Task (e.g. Create Project)')
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.name
+
+class Points(models.Model):
+    """Model representing a point to a Task."""
+    name = models.IntegerField(help_text='Enter a Value between 1 to 100')
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.name
